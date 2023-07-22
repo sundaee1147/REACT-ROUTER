6731c67d797ff7e8d10267176d89f6619ab0f7dc
sundaee1147 <Ogbodudavid10@gmail.com>
2023-07-22 11:47:13 +0100

    React Router

---
+++ router-app/src/Components/Description/Description.js
@@ -0,0 +1,25 @@
+import MovieCard from "../MovieCard/MovieCard";
+import "./Description.css";
+import {useParams} from "react-router-dom";
+
+
+
+export default function Description({list}) {
+
+    console.log(list.length);
+    let params = useParams();
+    return (
+        <div className="desc">
+            {  ( isNaN(params.id) || list.length <= parseInt(params.id)) ?  <p>No Movie With this id </p> :
+            
+            <>
+            <MovieCard ele={list[params.id]} /> 
+            <iframe width="560" height="315" src={list[params.id].trailer}
+                title="YouTube video player" frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen></iframe>
+            </>
+            }
+        </div>
+    )
+}
